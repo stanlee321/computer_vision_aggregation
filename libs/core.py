73d9d855cf19e801d798c23c4a0c54443c243fe0
Stanley Salvatierra <stanlee321@gmail.com>
2024-12-04 19:49:35 -0400

    fixed main initizalition

--- libs/core.py
+++ libs/core.py
@@ -1,27 +1,34 @@
 import os
 import re
 import pandas as pd
+import uuid
+
 from typing import Tuple, List
 
 from libs.queues import KafkaHandler
-from libs.redis_service import RedisClient
 from libs.api import ApiClient
-from libs.utils import (
-    put_to_redis,
-)
 
 from libs.video_handler import VideoHandler
 from libs.clean_data import ProcessData
 
 from minio import Minio
-
 class Application:
-    def __init__(self, server_ip: str, minio_access_key: str, minio_secret_key: str, brokers: List[str], api_base_url: str):
-        self.bucket_name = "my-bucket"
-        self.topic_input = 'video-results'
-        self.topic_fine_detections = 'fine-detections'
-        self.topic_group = 'results-group'
-        self.output_folder = './tmp'
+    def __init__(self, server_ip: str, 
+                 minio_access_key: str, 
+                 minio_secret_key: str, 
+                 brokers: List[str], 
+                 api_base_url: str, 
+                 topic_input: str,
+                 topic_output: str,
+                 bucket_name: str,
+                 output_folder: str
+                 ):
+        
+        
+        self.bucket_name = bucket_name
+        self.topic_input = topic_input
+        self.topic_output = topic_output
+        self.output_folder = output_folder
         self.data_handler = ProcessData()
         self.kafka_handler = KafkaHandler(bootstrap_servers=brokers)
 
@@ -30,7 +37,6 @@ class Application:
                                     secret_key= minio_secret_key,
                                     secure=False)
         self.video_handler = VideoHandler(output_folder=self.output_folder)
-        self.redis_client = RedisClient(host=f'{server_ip}', port=6379, password="Secret.")
         
         self.api_client = ApiClient(api_base_url)
         self.workdir = self.output_folder
@@ -44,6 +50,7 @@ class Application:
         self.workdir = f'{self.output_folder}/{video_id}'
         output_file_path = f'{self.workdir}/{filename}'
         return output_file_path
+    
     def get_working_data_file(self, chunk_number, max_chunks):
         return os.path.join(self.workdir, f'output_{chunk_number}_{max_chunks}.csv')
 
@@ -122,14 +129,9 @@ class Application:
         video_id: str = _message_input['video_id']
         
         tasks_list = self.create_main_tasks(video_id, remote_path)
-        
-        print("Tasks list: ", tasks_list)
-        # Chunks
-        # chunk_data = self.extract_chunk_and_annotated(remote_path)
-        # chunk_number = chunk_data['chunk']
-        # working_data_file  = self.get_working_data_file(chunk_number, max_chunks)
 
-        # max_chunks = chunk_data['total_chunks']
+        print("Tasks list: ", tasks_list)
+
         if tasks_list is None:
             return
 
@@ -143,27 +145,9 @@ class Application:
 
 
         working_data_file  = self.get_working_data_file(chunk_number, max_chunks)
-        # if chunk_number > 1:
-        #     print("Chunk number > 1")
-        #     if os.path.exists(working_data_file):
-        #         print("File exists")
-        #         df = self.data_handler.create_pandas_data(working_data_file)
-        #     else:
-        #         print("File does not exist")
-        #         # Load previous data chunks and join with this new one
-        #         working_data_file_previous  = self.get_working_data_file(chunk_number-1, max_chunks)
-        #         df_prev = self.data_handler.create_pandas_data(working_data_file_previous)
-                
-        #         # Create the new dataframe
-        #         df_new = self.create_main_dataframe(tasks_list, self.fps)
-                
-        #         df = pd.concat([df_prev, df_new])          
-        # else:
-            
-        df.to_csv(working_data_file, index=False)
-        
 
-        # video_id = "a29615c3-9227-496e-b688-839ad828c898"
+        df.to_csv(working_data_file, index=False)
+
         df = pd.read_csv(working_data_file)
         print("saving data to ...", working_data_file )
         
@@ -174,14 +158,15 @@ class Application:
             minio_client = self.client_minio, 
             bucket_name= self.bucket_name, 
             video_handler =self.video_handler)
-        
+
+        # Set the filenames
         local_file_results = self.data_handler.set_filenames(
             video_id=video_id, results_file_name = 'output_json_timestamp.json')
         
         local_file_results_full = self.data_handler.set_filenames(
             video_id=video_id, results_file_name = 'output_json_timestamp_full.json')
   
-        
+        # Create the json data
         json_data_full =  self.data_handler.create_json_data(
                                              df, 
                                              annotated_video = remote_video_path, 
@@ -189,20 +174,22 @@ class Application:
                                              output_path = local_file_results_full, 
                                              original_video = self.original_video,
                                              keep_columns = None)
-
+        
+        # Add the process to the json data
+        json_data_full['process'] = f"{chunk_number}/{max_chunks}"
+        
         # Save json_data_full to json file
         complete_data_path = os.path.join(self.workdir, 'complete_data.json')
-        key_data_full = put_to_redis(video_id, json_data_full, redis_client =  self.redis_client, label="complete", local_data_path = complete_data_path)
 
         # Upload complete data to minio
         self.client_minio.fput_object(self.bucket_name, f'{video_id}/complete_data.json', complete_data_path)
         
         
         # Send the data to the next topic
-        self.kafka_handler.produce_message(self.topic_fine_detections, 
-                                            { 
-                                               "full_data": key_data_full
-                                            })
+        # self.kafka_handler.produce_message(self.topic_fine_detections, 
+        #                                     { 
+        #                                        "full_data": key_data_full
+        #                                     })
         
         # update tasks by id in df['id']
         ids_to_update = self.df_tasks['id'].tolist()
@@ -210,12 +197,17 @@ class Application:
         for id in ids_to_update:
             self.api_client.update_item_status(id, update_data)
             
-    def run(self):
+    def generate_uuid(self):
+        return str(uuid.uuid4())
+
+    def run(self, offset: str = 'latest'):
         print("Consuming topic. {}".format(self.topic_input) ,)
-        print("Group: {}".format(self.topic_group))
-        consumer = self.kafka_handler.create_consumer(self.topic_input, 
-                                                        self.topic_group, 
-                                                        auto_offset_reset='earliest')
+        group_id = 'video-aggregator-' + self.generate_uuid()
+
+        consumer = self.kafka_handler.create_consumer(self.topic_input,
+                                                      group_id=group_id,
+                                                      auto_offset_reset=offset)
+
         for message in consumer:
             print(f"Consumed message: {message.value}")
             self.process_message(message)
--- main.py
+++ main.py
@@ -1,18 +1,34 @@
+import os
 from libs.core import Application
-SERVER_IP = "localhost"
-API_BASE_URL = "http://localhost:8003"
-minio_key =  "WraSmXSjxqJgFgPSklHk"
-minio_secret = "Jt4AeVlUVcLZmrhxaRDcABTx2Ir3ODlmxxuXLnsF"
-minio_url = f"{SERVER_IP}:9000"
 
-# Kafka
+
+SERVER_IP = "192.168.1.19"
+API_BASE_URL = f"http://{SERVER_IP}:8003"
+
+minio_key = "c3aFDmKuGhPCSxkpRDGf"
+minio_secret = "MLYz9tZI3h4xZAwBl8llyEtX6R07YcMuRdSYPIcx"
+minio_url = f"{SERVER_IP}:9000"
+BUCKET_NAME = "my-bucket"
+
+
+TOPIC_INPUT = "video-general-results"
+TOPIC_OUTPUT = "video-fine-detections"
 brokers = [f'{SERVER_IP}:9092']
+
+WORKING_FOLDER = "./tmp"
 
 if __name__ == "__main__":
     print("Starting...")
+    
+    os.makedirs(WORKING_FOLDER, exist_ok=True)
+    
     app = Application(server_ip=SERVER_IP,
                       brokers=brokers,
                       minio_access_key=minio_key, 
                       minio_secret_key=minio_secret,
-                      api_base_url=API_BASE_URL)
+                      api_base_url=API_BASE_URL,
+                      topic_input=TOPIC_INPUT,
+                      topic_output=TOPIC_OUTPUT,
+                      bucket_name=BUCKET_NAME,
+                      output_folder=WORKING_FOLDER)
     app.run()
